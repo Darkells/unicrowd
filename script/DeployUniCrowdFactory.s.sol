da6295fc5495689a6d4e60916ee80456a8b944f3
Draken <951086294@qq.com>
2025-08-16 01:36:06 +0800

    fmt

--- script/DeployUniCrowdFactory.s.sol
+++ script/DeployUniCrowdFactory.s.sol
@@ -8,21 +8,14 @@ contract DeployUniCrowdFactory is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
-        
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         UniCrowdFactory factory = new UniCrowdFactory();
-        
+
         vm.stopBroadcast();
-        
+
         console.log("UniCrowdFactory deployed at:", address(factory));
         console.log("Deployer address:", deployer);
-        
-        // 输出部署信息供前端使用
-        console.log("=== Deployment Info ===");
-        console.log("Network: Anvil Local");
-        console.log("Chain ID: 31337");
-        console.log("Factory Address:", address(factory));
-        console.log("Deployer:", deployer);
     }
 }
